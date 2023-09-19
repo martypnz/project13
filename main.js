9d13d85a8ef036c82bb4be0d0bde8e46772d5157
martypnz <141710574+martypnz@users.noreply.github.com>
2023-09-19 20:40:31 +1200

    functions

--- main.js
+++ main.js
@@ -1,29 +1,31 @@
-//document.querySelector('h1')
-//document.querySelector('#heading')
-//document.querySelector('.header')
-//document.querySelector('h1.header')
+console.log("hello world");
+//function or methods
 
-//document.querySelector("[type='text']")
+function myFirstFunction() {
+  console.log("this text is from inside function");
+}
 
-const h1= document.querySelector('h1')
-h1.style.color='red'
-h1.style.FontSize='4em'
-h1.style.textAlign='center'
+//this function updates the styling of my main heading
+function changeMyHeading() {
+  const h1 = document.querySelector("h1");
+  h1.style.color = "#f11";
+  h1.style.backgroundColor = "#ddd";
+  myFirstFunction();
+}
 
-const myForm = document.querySelector('form')
-myForm.setAttribute('id','form')
-myForm.setAttribute('class','my-random-class')
+changeMyHeading();
 
-const body=document.querySelector('body')
+function addNewElement(){
+    const element = document.createElement('p');
+    element.textContent= 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. A, quidem.'
+    document.body.appendChild(element)
+}
 
-//h1.classList.remove('heading')
+addNewElement()
 
-const h2= document.createElement('h2')
-//const body= document.body;
-h2.innerHTML='you just created this in javascript'
-h2.classList.add('subheading')
-document.body.appendChild(h2)
-
-
-const FormButton =document.querySelector('[type="submit"]')
-FormButton.style.setProperty('background-color','red')
+function addInputToForm(){
+    const form = document.querySelector('form')
+    const input = document.createElement('input')
+    input.setAttribute('type','color')
+    form.appendChild(input)
+}
